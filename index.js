a36526eea6af2142814ec6594fea0971d4772a5f
Sam Parsons <samparsons269@gmail.com>
2021-05-02 13:42:37 -0400

    added function overload for string and function inputs, error handling

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 module.exports = function (arg1, options) {
   // check if Sanitizer API exists in window
+  if (!('Sanitizer' in window)) {
+    return new ReferenceError('Sanitizer API does not exist in this browser');
+  }
   // if arg1 is string, then return sanitized string
   if (typeof arg1 === 'string') {
     const san = new window.Sanitizer();
     return san.sanitizeToString(arg1);
   }
   // if arg1 is function, return event handler function with e.target.sanitizedValue
+  else if (typeof arg1 === 'function') {
+    const callback = arg1;
+    const san = new window.Sanitizer();
+    return function (e) {
+      e.target.sanitizedValue = san.sanitizeToString(e.target.value);
+      callback(e);
+    };
+  }
+  return new Error('Unrecognized input data type, use a string or function.');
 };
 
 // function overload
