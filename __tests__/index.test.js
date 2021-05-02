2acd57eae56270380f9d23445c6806b6caa4b81c
Sam Parsons <samparsons269@gmail.com>
2021-05-02 14:16:09 -0400

    using Sanitizer mock

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,13 @@
 const Sanitizer = require('../index.js');
+const SanitizerMock = require('../__mocks__/Sanitizer.mock');
 
 test('should return string given string argument', () => {
+  Object.defineProperty(window, 'Sanitizer', {
+    writable: true,
+    value: SanitizerMock,
+  });
+
   expect(Sanitizer('this is a string')).toBe('this is a string');
 });
+
 // how to mock a DOM API in jest?
