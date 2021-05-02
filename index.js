266438aefd27b8d87a0f2b745f645142a790cdba
Sam Parsons <samparsons269@gmail.com>
2021-05-02 15:40:29 -0400

    commented, formatted, and refactored index

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-module.exports = function (arg1, options) {
+module.exports = function (arg, options) {
   // check if Sanitizer API exists in window
   if (!('Sanitizer' in window)) {
     return new ReferenceError('Sanitizer API does not exist in this browser');
   }
-  // if arg1 is string, then return sanitized string
-  if (typeof arg1 === 'string') {
+
+  /**
+   * Object.assign default options with options argument
+   * // Options argument to new window.Sanitizer() is not standardized
+   */
+
+  // if arg is string, then return sanitized string
+  if (typeof arg === 'string') {
     const san = new window.Sanitizer();
-    return san.sanitizeToString(arg1);
+    return san.sanitizeToString(arg);
   }
-  // if arg1 is function, return event handler function with e.target.sanitizedValue
-  else if (typeof arg1 === 'function') {
-    const callback = arg1;
+  // if arg is function, return event handler function with e.target.sanitizedValue
+  else if (typeof arg === 'function') {
+    const callback = arg;
     const san = new window.Sanitizer();
     return function (e) {
       e.target.sanitizedValue = san.sanitizeToString(e.target.value);
       callback(e);
     };
   }
+
+  // catch invalid arg type
   return new Error('Unrecognized input data type, use a string or function.');
 };
-
 // function overload
