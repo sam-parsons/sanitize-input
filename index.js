a63b6603ec9aefa8adeca33fc6b74f5f8d26e953
Sam Parsons <samparsons269@gmail.com>
2021-05-02 12:57:44 -0400

    initial working commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = function () {
-  return 'sanitized input';
+module.exports = function (arg1, options) {
+  // check if Sanitizer API exists in window
+  // if arg1 is string, then return sanitized string
+  if (typeof arg1 === 'string') {
+    const san = new window.Sanitizer();
+    return san.sanitizeToString(arg1);
+  }
+  // if arg1 is function, return event handler function with e.target.sanitizedValue
 };
+
+// function overload
